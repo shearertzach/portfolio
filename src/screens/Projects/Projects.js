83bf9dca2787f71dbad418f0dd203e1ffcecb572
shearertzach <shearertzach@gmail.com>
2021-01-12 19:08:27 -0600

    Made Changes

--- src/screens/Projects/Projects.js
+++ src/screens/Projects/Projects.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import allProjects from '../../data/projects.json'
 import './Projects.css'
+import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 export default function Projects() {
 
     function renderProjects() {
-        return allProjects.map((project) => {
+        return allProjects.map((project, i) => {
             return (
-                <div className='projectCard'>
+                <motion.div
+                    className='projectCard'
+                    initial={{ opacity: 0, y: 300 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: i, type: "spring", stiffness: 100 }}
+                >
                     <a href={project['external-link']}><p>{project.name}</p></a>
+                    <img src={project['img']} alt='placeholderimg' />
                     <p>{project.description}</p>
-                </div>
+                </motion.div>
             )
         })
     }
 
     return (
         <div className='projects'>
-            {renderProjects()}
+            <Link to='/' className='backButton'>← Back</Link>
+            <div className='projectsContainer'>
+                {renderProjects()}
+            </div>
         </div>
     )
 }
